fed24ed47e144f36fa009dac79ae7c51c8178a1c
leetgrind <leetgrind.org@gmail.com>
2024-07-16 08:52:35 -0400

    socket react

--- socket/index.js
+++ socket/index.js
@@ -1,12 +1,17 @@
 const express = require("express");
 const http = require("http");
 const {Server} = require("socket.io");
-
+const cors = require("cors");
 
 const app = express();
+app.use(cors());
 const server = http.createServer(app);
 
-const io = new Server(server);
+const io = new Server(server, {
+    cors: {
+        origin: 'http://localhost:3000'
+    }
+});
 
 io.on('connection', (socket) => {
     console.log(`${socket.id} - user connected`);
