025d7861ac61f31a13b15acde822183f16598f01
leetgrind <leetgrind.org@gmail.com>
2024-07-16 09:28:44 -0400

    added rooms on frontend

--- chat_frontend/src/pages/Rooms.js
+++ chat_frontend/src/pages/Rooms.js
@@ -4,6 +4,7 @@ import { socket } from "../config/socket"
 export default function Rooms() {
     const [isConnected, setIsConnected] = useState(socket.connected);
     const [username, setUsername] = useState('');
+    const [room, setRoom] = useState('');
 
     useEffect(() => {
         function onConnect() {
@@ -26,11 +27,19 @@ export default function Rooms() {
 
 
     return (
-        <div>
-            Server connected: {isConnected}
-            <h1 className="text-2xl text-slate-400">Enter your username</h1>
+        <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12">
+            <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+                <h1 className="text-center text-2xl font-bold tracking-tight text-slate-400">Select a chatroom</h1>
+                <p className="text-center text-lg tracking-tight text-slate-400">Enter username</p>
             <input id="username" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)}/>
-            <button onClick={loginToChat}>Submit</button>
+            <select onChange={(e) => setRoom(e.target.value)}>
+                <option>---Select Room ---</option>
+                <option value='js'>Javascript</option>
+                <option value='java'>Java</option>
+                <option value='python'>Python</option>
+            </select>
+            <button className="flex items-center justify-center rounded-md bg-indigo-500 px-5 text-white" onClick={loginToChat}>Submit</button>
+            </div>
         </div>
     )
 }
